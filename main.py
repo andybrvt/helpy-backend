391b8f53cb5ed001831ab33df859ee7eaa502b5d
Andy Le <andybrvt@gmail.com>
2024-09-22 23:05:09 -0700

    cors

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from routers import user  # Import the user router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 # post, put, delete
 # authentication
@@ -7,6 +9,18 @@ from routers import user  # Import the user router
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",  # React app URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 @app.get("/")
 def read_root():
